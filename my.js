2c4ce244ab20f56082dcb2b536d77960e2c1476b
Laura <32312667+la558@users.noreply.github.com>
2020-06-20 18:33:00 -0400

    Update my.js

--- my.js
+++ my.js
@@ -10,5 +10,24 @@ function testGS() {
         });
 
 }
+function addGS() {
+// here we are going to be doing a 'post' request
+    var url = "https://script.google.com/macros/s/AKfycbzm99ObzBPz6rr0wAi2d7fgaEiojZaimIzk53Ce_zDT_26eTKk/exec";
+
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'omit', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:'Olivia'}) // body data type must match "Content-Type" header
+      });
+
+}
 
 document.getElementById('btn').addEventListener('click', testGS);
+document.getElementById('btn2').addEventListener('click', addGS);
