b2c5b2a3c8a34c8581c635bcd53be31ccc4a775d
Laura <32312667+la558@users.noreply.github.com>
2020-06-23 16:16:18 -0400

    Update my.js

--- my.js
+++ my.js
@@ -1,15 +1,17 @@
 function testGS() {
 
-    var url = "https://script.googleusercontent.com/a/macros/georgetown.edu/echo?user_content_key=Mjzb_UwlB6vmKmMFCglVFIcasSOMpTR5788Shrlu5PHFb8zMBljmFu8xLKciYv5gnTFPpp6CgGhiRn2p1S_-E9BXM6TD7_YQm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_somEH0VO9G8l_mT0SNpLRpbGYfaIuFVoS4chwneYR2vobCuPsKWDSKwSQoG6olLcnPKcB2RudzpEmCV3zilwYMOpyR1BoIqbkaSCh4rR6161liWvYgWzoA&lib=Mk-4mRBgqOKBWGnxUViSKe9Y7yp0QSEB_";
+     // const url = "https://script.googleusercontent.com/a/macros/georgetown.edu/echo?user_content_key=Mjzb_UwlB6vmKmMFCglVFIcasSOMpTR5788Shrlu5PHFb8zMBljmFu8xLKciYv5gnTFPpp6CgGhiRn2p1S_-E9BXM6TD7_YQm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_somEH0VO9G8l_mT0SNpLRpbGYfaIuFVoS4chwneYR2vobCuPsKWDSKwSQoG6olLcnPKcB2RudzpEmCV3zilwYMOpyR1BoIqbkaSCh4rR6161liWvYgWzoA&lib=Mk-4mRBgqOKBWGnxUViSKe9Y7yp0QSEB_";
+     const url = "https://script.google.com/macros/s/AKfycbzm99ObzBPz6rr0wAi2d7fgaEiojZaimIzk53Ce_zDT_26eTKk/exec"
 
     fetch(url)
-        .then(d => d.json())
-        .then(d => {
-            document.getElementById('app').textContent = d[0].status;
+        .then(d => d.json()) // get the response and converted into json
+        .then(d => {         // 'd' is the response >> array with status 'cool'
+            document.getElementById('app').textContent = d[0].status;  // we add the response into the app id
     
         });
 
 }
+
 function addGS() {
 // here we are going to be doing a 'post' request
     var url = "https://script.google.com/macros/s/AKfycbzm99ObzBPz6rr0wAi2d7fgaEiojZaimIzk53Ce_zDT_26eTKk/exec";
@@ -24,10 +26,10 @@ function addGS() {
         },
         redirect: 'follow', // manual, *follow, error
         // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({name:'Joe'}) // body data type must match "Content-Type" header
+        body: JSON.stringify({name:'Olivia'}) // body data type must match "Content-Type" header
       });
 
 }
 
-document.getElementById('btn').addEventListener('click', testGS);
+document.getElementById('btn').addEventListener('click', testGS);  // this button will trigger the function testGS 'on click'
 document.getElementById('btn2').addEventListener('click', addGS);
